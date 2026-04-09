039e1d371639d662dfa6d83a6712bae01846a512
maazkhan551 <bakhthafiz1@gmail.com>
2026-04-09 09:18:12 +0500

    Practice redux toolkit

---
+++ Redux-toolKit/src/App.jsx
@@ -0,0 +1,15 @@
+import TodoForm from "./components/TodoForm"
+import Todos from "./components/Todos"
+
+ 
+function App() { 
+
+  return (
+    <>
+      <TodoForm/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
